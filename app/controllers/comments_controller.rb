97fa5780a71cef7e49c57fed816c8b70649c424e
astreiko <astreika1.19@gmail.com>
2020-03-31 09:58:37 +0300

    CommentController

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,8 +3,9 @@ class CommentsController < ApplicationController
   def create
     @item = Item.find(params[:item_id])
     @group = Group.find(@item.group_id)
-    @comment = @item.comments.create(comment_params)
+    @comment = Comment.new(comment_params)
     @comment.user_id = current_user.id
+    @comment.item_id = @item.id
     if @comment.save
         redirect_to showOne_item_path(:id => @item.id, :group_id => @group.id)
     end
