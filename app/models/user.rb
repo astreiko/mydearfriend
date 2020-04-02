feeb3582d918ba0f8229abc4dfdb6f0f0d816f81
astreiko <astreika1.19@gmail.com>
2020-04-02 17:08:27 +0300

    user.rb omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,8 +13,9 @@ class User < ApplicationRecord
 
 
 def self.from_omniauth(auth)
-  where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-    user.email = auth.info.email
+  where(email: auth.info.email).first_or_create do |user|
+    user.provider = auth.provider
+    user.uid = auth.uid
     user.password = Devise.friendly_token[0, 20]
     user.name = auth.info.name   # assuming the user model has a name
   end
